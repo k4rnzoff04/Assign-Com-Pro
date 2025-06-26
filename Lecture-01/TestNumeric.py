d15b327d24a07f19782903a23e003076636461fe
k4rnzoff04 <k4rnzoff04@gmail.com>
2025-06-26 17:02:30 +0700

    EndCommit

---
+++ Lecture-01/TestNumeric.py
@@ -0,0 +1,23 @@
+from math import pi
+
+# print('100.00')
+# print('%d' % 100)
+# print('%d' % 100.58)
+# print('%f' %-100.89)
+# print('%.2f' % 100.89)
+
+print('%.2f' % pi)
+
+print('%.4f' % pi)
+
+print('%.50f' % pi)
+
+print('My age is', 25 , 'I have', 3500.50, 'Bth.')
+print('My age is %d I have %.2f Bth.' % (25, 3500.50))
+print('My age is ' + str(25) + ' I have ' + str(3500.50) + ' Bth.')
+
+print(' 5 + 4 =', 5+4)
+
+print(' 5 + 4 = %.2f'% (5+4))
+
+print(' 5 + 4 = ' + str(5+4))
