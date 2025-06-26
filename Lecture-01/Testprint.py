bbd70a1ba7ea99d3d41a0bad62e07719ca975f44
k4rnzoff04 <k4rnzoff04@gmail.com>
2025-06-26 16:37:22 +0700

    FirstCommit

---
+++ Lecture-01/Testprint.py
@@ -0,0 +1,8 @@
+# print("Hello World")
+# print("Peerakan Kraipinij")
+# print("This is my first Python program")
+# print("I'm Hungry")
+# print("I'm good and \"I will do my mind best\"")
+# print("/\\")
+# print("\ta")
+
