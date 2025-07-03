9eb5be0e0db51c6bcbe90d1c6b77f9520365576f
k4rnzoff04 <k4rnzoff04@gmail.com>
2025-07-03 15:21:58 +0700

    Save Lecture2

--- Lecture-02/practical_examples.py
+++ Lecture-02/practical_examples.py
@@ -1,19 +1,19 @@
-# #Example I
-# age = input("Enter your age: ")
-# age = int(age)
+#Example I
+age = input("Enter your age: ")
+age = int(age)
 
-# height = input("Enter your height: ")
-# height = float(height)
-# print("You are " + str(age) + " years old and " + str(height) + " feet tall.")
-# #หรือ
-# print(f"You are {str(age)} years old and {str(height)} feet tall.")
+height = input("Enter your height: ")
+height = float(height)
+print("You are " + str(age) + " years old and " + str(height) + " feet tall.")
+#หรือ
+print(f"You are {str(age)} years old and {str(height)} feet tall.")
 
-# # Example II
-# weight = float(input("Enter your weight in kilograms: "))
-# height = float(input("Enter your height in meters: "))
+# Example II
+weight = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in meters: "))
 
-# bmi = weight / (height ** 2)
-# print(f"Your BMI is {bmi:.6f}")
+bmi = weight / (height ** 2)
+print(f"Your BMI is {bmi:.6f}")
 
 #Example III
 celsius = float(input("Enter temerature in Celsius: "))
--- Lecture-02/test_conversion.py
+++ Lecture-02/test_conversion.py
@@ -1,24 +1,24 @@
-# age = 25
-# height = float(age)
-# print(height)
+age = 25
+height = float(age)
+print(height)
 
-# height = 5.6
-# age = int(height)
-# print(age)
+height = 5.6
+age = int(height)
+print(age)
 
-# num_str = "123"
-# num = int(num_str)
-# print(num)
+num_str = "123"
+num = int(num_str)
+print(num)
 
-# x = 15
-# y = 4
+x = 15
+y = 4
 
-# print("x + y = ", x + y)
-# print("x - y = ", x - y)
-# print("x * y = ", x * y)
-# print("x / y = ", x / y)
-# print("x % y = ", x % y)
-# print("x // y = ", x // y)
-# print("x ** y = ", x ** y)
+print("x + y = ", x + y)
+print("x - y = ", x - y)
+print("x * y = ", x * y)
+print("x / y = ", x / y)
+print("x % y = ", x % y)
+print("x // y = ", x // y)
+print("x ** y = ", x ** y)
 
-# print ("5 + 8 // 2 - 25 % 4 = ", 5 + (8 // 2) - (25 % 4))
+print ("5 + 8 // 2 - 25 % 4 = ", 5 + (8 // 2) - (25 % 4))
