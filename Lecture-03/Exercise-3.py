60b220007911d5922fceb5e7a1fec27b22b41348
k4rnzoff04 <k4rnzoff04@gmail.com>
2025-07-07 11:43:45 +0700

    End commit

--- Lecture-03/Exercise-3.py
+++ Lecture-03/Exercise-3.py
@@ -1,6 +1,13 @@
 h_worked = int(input("Enter the number of hours worked: "))
 pay_rate = float(input("Enter the pay rate per: "))
 
-gross_pay = h_worked * pay_rate
 
-print(f"The gross pay is ${gross_pay:,.2f}")
+if h_worked > 40:
+    gross_pay = ((h_worked - 40) *1.5*pay_rate) + (40 * pay_rate)
+    
+else:
+    gross_pay = h_worked * pay_rate
+
+
+
+print(f"The gross pay is ${gross_pay}")
