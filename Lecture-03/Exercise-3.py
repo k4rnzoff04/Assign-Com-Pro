5c1654e10254d2754642b68f5aec03900dffc3ce
k4rnzoff04 <k4rnzoff04@gmail.com>
2025-07-07 11:48:15 +0700

    End commit

--- Lecture-03/Exercise-3.py
+++ Lecture-03/Exercise-3.py
@@ -3,7 +3,9 @@ pay_rate = float(input("Enter the pay rate per: "))
 
 
 if h_worked > 40:
-    gross_pay = ((h_worked - 40) *1.5*pay_rate) + (40 * pay_rate)
+    # gross_pay = ((h_worked - 40) *1.5*pay_rate) + (40 * pay_rate)
+    overtime_hours = h_worked - 40
+    gross_pay = (overtime_hours * (1.5 * pay_rate) + (40 * pay_rate))
     
 else:
     gross_pay = h_worked * pay_rate
